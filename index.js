a0e86089aa94d4ef3843738a43ee8fbcdf72a189
Louis Suo <louis.suo@myob.com>
2017-01-09 15:46:44 +1100

    init repo

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const Rx = require('rxjs');
+
+let source = Rx.Observable.interval(400).take(9)
+  .map(i => ['1', '2', '3', 'foo', 'bar', '8', '13'][i]);
+
+const result = source
+  .map(x => parseInt(x))
+  .filter(x => !isNaN(x))
+  .reduce((x, y) => x + y);
+
+result.subscribe(console.log);
