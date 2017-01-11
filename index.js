e97c74cfb755718f0b4ecc4b12ec129f9ed83e8e
Louis Suo <louis.suo@myob.com>
2017-01-11 16:31:40 +1100

    load api content and display

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Rx = require('rxjs');
+const _ = require('lodash');
 
 let source = Rx.Observable.interval(400).take(9)
   .map(i => ['1', '2', '3', 'foo', 'bar', '8', '13'][i]);
@@ -28,6 +29,33 @@ $(document).ready(() => {
   doubleClickStream.subscribe(event => label.text('button is double clicked!!'));
 
   doubleClickStream.delay(1000).subscribe(suggestion => label.text('-'));
+
+  const requestStream = Rx.Observable.of('https://api.github.com/users');
+  const responseStream = requestStream
+    .flatMap(url => Rx.Observable.fromPromise($.getJSON(url)));
+
+  responseStream.subscribe(res => {
+    console.log(res);
+  })
+
+  const suggestionStream1 = createSuggestionStream(responseStream);
+  const suggestionStream2 = createSuggestionStream(responseStream);
+  const suggestionStream3 = createSuggestionStream(responseStream);
+
+
+  suggestionStream1.subscribe(_.curry(renderSuggestion)('.suggestion1'));
+  suggestionStream2.subscribe(_.curry(renderSuggestion)('.suggestion2'));
+  suggestionStream3.subscribe(_.curry(renderSuggestion)('.suggestion3'));
 });
 
+const createSuggestionStream = responseStream => responseStream.map(listUser => listUser[Math.floor(Math.random() * listUser.length)]);
+
+const renderSuggestion = (selector, userData) => {
+  let userElem = $('.username', selector);
+  console.log('userElem', userElem);
+  userElem.attr('href', userData.html_url);
+  userElem.text(userData.login);
+  const imgElem = $('img', selector);
+  imgElem.attr('src', userData.avatar_url);
+}
 
