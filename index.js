6d776b6f5c76e4140b647c921c2026d43b4ae75c
Louis Suo <louis.suo@myob.com>
2017-01-11 15:02:46 +1100

    stream dom events

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ const result = source
   .reduce((x, y) => x + y);
 
 result.subscribe(console.log);
+
+$(document).ready(() => {
+  const button = $('.button');
+  const label = $('.text');
+  console.log(`button is ${button.textContent}`);
+  console.log(`text is ${label}`);
+
+  let clickStream = Rx.Observable.fromEvent(button, 'click');
+
+  const doubleClickStream = clickStream
+    .bufferWhen(() => clickStream.debounceTime(250))
+    .map(arr => arr.length)
+    .filter(len => len === 2);
+
+  doubleClickStream.subscribe(event => label.text('double clicked!'));
+
+  doubleClickStream.delay(1000).subscribe(suggestion => label.text('-'));
+});
+
+
